a21d76871501689fa6263fe00cefb63d1bac0dcb
AKASHRAWAT45 <akash.rawat24.ar@gmail.com>
2022-12-25 09:16:03 +0530

    initial commit

---
+++ calculator-automate/calculatornew/TestFixture1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace calculatornew
+{
+    partial class TestFixture1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestFixture1));
+            this.CalculatorApplication = new TestAutomationFX.UI.UIApplication();
+            this.Form1 = new TestAutomationFX.UI.UIWindow();
+            this.Button15 = new TestAutomationFX.UI.UIWindow();
+            this.Button14 = new TestAutomationFX.UI.UIWindow();
+            this.Button12 = new TestAutomationFX.UI.UIWindow();
+            this.Button10 = new TestAutomationFX.UI.UIWindow();
+            this.Button9 = new TestAutomationFX.UI.UIWindow();
+            this.Button7 = new TestAutomationFX.UI.UIWindow();
+            this.Button1 = new TestAutomationFX.UI.UIWindow();
+            this.Button2 = new TestAutomationFX.UI.UIWindow();
+            this.Button4 = new TestAutomationFX.UI.UIWindow();
+            this.Button20 = new TestAutomationFX.UI.UIWindow();
+            this.Button3 = new TestAutomationFX.UI.UIWindow();
+            this.Button8 = new TestAutomationFX.UI.UIWindow();
+            this.Button13 = new TestAutomationFX.UI.UIWindow();
+            this.Button18 = new TestAutomationFX.UI.UIWindow();
+            this.Button16 = new TestAutomationFX.UI.UIWindow();
+            this.Button6 = new TestAutomationFX.UI.UIWindow();
+            this.TextBox_Result = new TestAutomationFX.UI.UIWindow();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // CalculatorApplication
+            // 
+            this.CalculatorApplication.Comment = null;
+            this.CalculatorApplication.ImagePath = "D:\\Calculator-master\\Calculator-master\\Calculator.exe";
+            this.CalculatorApplication.Name = "CalculatorApplication";
+            this.CalculatorApplication.ObjectImage = null;
+            this.CalculatorApplication.Parent = null;
+            this.CalculatorApplication.ProcessName = "Calculator";
+            this.CalculatorApplication.TimeOut = 1000;
+            this.CalculatorApplication.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Application;
+            this.CalculatorApplication.UseCoordinatesOnClick = false;
+            this.CalculatorApplication.UsedMatchedProperties = ((TestAutomationFX.UI.UIApplication.UIApplicationMatchedProperties)((TestAutomationFX.UI.UIApplication.UIApplicationMatchedProperties.ProcessName | TestAutomationFX.UI.UIApplication.UIApplicationMatchedProperties.CommandLineArguments)));
+            this.CalculatorApplication.WorkingDirectory = "D:\\Calculator-master\\Calculator-master";
+            // 
+            // Form1
+            // 
+            this.Form1.Comment = null;
+            this.Form1.InstanceName = "Form1";
+            this.Form1.MatchedIndex = 0;
+            this.Form1.MsaaName = "Calculator";
+            this.Form1.MsaaRole = System.Windows.Forms.AccessibleRole.Client;
+            this.Form1.Name = "Form1";
+            this.Form1.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Form1.ObjectImage")));
+            this.Form1.OwnedWindow = true;
+            this.Form1.Parent = this.CalculatorApplication;
+            this.Form1.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Window;
+            this.Form1.UseCoordinatesOnClick = true;
+            this.Form1.WindowClass = "";
+            // 
+            // Button15
+            // 
+            this.Button15.Comment = null;
+            this.Button15.Index = 9;
+            this.Button15.InstanceName = "button15";
+            this.Button15.MatchedIndex = 0;
+            this.Button15.MsaaName = "1";
+            this.Button15.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button15.Name = "Button15";
+            this.Button15.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button15.ObjectImage")));
+            this.Button15.Parent = this.Form1;
+            this.Button15.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button15.UseCoordinatesOnClick = false;
+            this.Button15.WindowClass = "";
+            this.Button15.WindowText = "1";
+            // 
+            // Button14
+            // 
+            this.Button14.Comment = null;
+            this.Button14.Index = 8;
+            this.Button14.InstanceName = "button14";
+            this.Button14.MatchedIndex = 0;
+            this.Button14.MsaaName = "2";
+            this.Button14.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button14.Name = "Button14";
+            this.Button14.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button14.ObjectImage")));
+            this.Button14.Parent = this.Form1;
+            this.Button14.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button14.UseCoordinatesOnClick = false;
+            this.Button14.WindowClass = "";
+            this.Button14.WindowText = "2";
+            // 
+            // Button12
+            // 
+            this.Button12.Comment = null;
+            this.Button12.Index = 6;
+            this.Button12.InstanceName = "button12";
+            this.Button12.MatchedIndex = 0;
+            this.Button12.MsaaName = "3";
+            this.Button12.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button12.Name = "Button12";
+            this.Button12.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button12.ObjectImage")));
+            this.Button12.Parent = this.Form1;
+            this.Button12.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button12.UseCoordinatesOnClick = false;
+            this.Button12.WindowClass = "";
+            this.Button12.WindowText = "3";
+            // 
+            // Button10
+            // 
+            this.Button10.Comment = null;
+            this.Button10.Index = 14;
+            this.Button10.InstanceName = "button10";
+            this.Button10.MatchedIndex = 0;
+            this.Button10.MsaaName = "4";
+            this.Button10.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button10.Name = "Button10";
+            this.Button10.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button10.ObjectImage")));
+            this.Button10.Parent = this.Form1;
+            this.Button10.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button10.UseCoordinatesOnClick = false;
+            this.Button10.WindowClass = "";
+            this.Button10.WindowText = "4";
+            // 
+            // Button9
+            // 
+            this.Button9.Comment = null;
+            this.Button9.Index = 13;
+            this.Button9.InstanceName = "button9";
+            this.Button9.MatchedIndex = 0;
+            this.Button9.MsaaName = "5";
+            this.Button9.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button9.Name = "Button9";
+            this.Button9.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button9.ObjectImage")));
+            this.Button9.Parent = this.Form1;
+            this.Button9.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button9.UseCoordinatesOnClick = false;
+            this.Button9.WindowClass = "";
+            this.Button9.WindowText = "5";
+            // 
+            // Button7
+            // 
+            this.Button7.Comment = null;
+            this.Button7.Index = 11;
+            this.Button7.InstanceName = "button7";
+            this.Button7.MatchedIndex = 0;
+            this.Button7.MsaaName = "6";
+            this.Button7.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button7.Name = "Button7";
+            this.Button7.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button7.ObjectImage")));
+            this.Button7.Parent = this.Form1;
+            this.Button7.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button7.UseCoordinatesOnClick = false;
+            this.Button7.WindowClass = "";
+            this.Button7.WindowText = "6";
+            // 
+            // Button1
+            // 
+            this.Button1.Comment = null;
+            this.Button1.Index = 19;
+            this.Button1.InstanceName = "button1";
+            this.Button1.MatchedIndex = 0;
+            this.Button1.MsaaName = "7";
+            this.Button1.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button1.Name = "Button1";
+            this.Button1.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button1.ObjectImage")));
+            this.Button1.Parent = this.Form1;
+            this.Button1.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button1.UseCoordinatesOnClick = false;
+            this.Button1.WindowClass = "";
+            this.Button1.WindowText = "7";
+            // 
+            // Button2
+            // 
+            this.Button2.Comment = null;
+            this.Button2.Index = 18;
+            this.Button2.InstanceName = "button2";
+            this.Button2.MatchedIndex = 0;
+            this.Button2.MsaaName = "8";
+            this.Button2.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button2.Name = "Button2";
+            this.Button2.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button2.ObjectImage")));
+            this.Button2.Parent = this.Form1;
+            this.Button2.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button2.UseCoordinatesOnClick = false;
+            this.Button2.WindowClass = "";
+            this.Button2.WindowText = "8";
+            // 
+            // Button4
+            // 
+            this.Button4.Comment = null;
+            this.Button4.Index = 16;
+            this.Button4.InstanceName = "button4";
+            this.Button4.MatchedIndex = 0;
+            this.Button4.MsaaName = "9";
+            this.Button4.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button4.Name = "Button4";
+            this.Button4.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button4.ObjectImage")));
+            this.Button4.Parent = this.Form1;
+            this.Button4.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button4.UseCoordinatesOnClick = false;
+            this.Button4.WindowClass = "";
+            this.Button4.WindowText = "9";
+            // 
+            // Button20
+            // 
+            this.Button20.Comment = null;
+            this.Button20.Index = 5;
+            this.Button20.InstanceName = "button20";
+            this.Button20.MatchedIndex = 0;
+            this.Button20.MsaaName = "0";
+            this.Button20.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button20.Name = "Button20";
+            this.Button20.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button20.ObjectImage")));
+            this.Button20.Parent = this.Form1;
+            this.Button20.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button20.UseCoordinatesOnClick = false;
+            this.Button20.WindowClass = "";
+            this.Button20.WindowText = "0";
+            // 
+            // Button3
+            // 
+            this.Button3.Comment = null;
+            this.Button3.Index = 17;
+            this.Button3.InstanceName = "button3";
+            this.Button3.MatchedIndex = 0;
+            this.Button3.MsaaName = "/";
+            this.Button3.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button3.Name = "Button3";
+            this.Button3.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button3.ObjectImage")));
+            this.Button3.Parent = this.Form1;
+            this.Button3.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button3.UseCoordinatesOnClick = false;
+            this.Button3.WindowClass = "";
+            this.Button3.WindowText = "/";
+            // 
+            // Button8
+            // 
+            this.Button8.Comment = null;
+            this.Button8.Index = 12;
+            this.Button8.InstanceName = "button8";
+            this.Button8.MatchedIndex = 0;
+            this.Button8.MsaaName = "*";
+            this.Button8.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button8.Name = "Button8";
+            this.Button8.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button8.ObjectImage")));
+            this.Button8.Parent = this.Form1;
+            this.Button8.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button8.UseCoordinatesOnClick = false;
+            this.Button8.WindowClass = "";
+            this.Button8.WindowText = "*";
+            // 
+            // Button13
+            // 
+            this.Button13.Comment = null;
+            this.Button13.Index = 7;
+            this.Button13.InstanceName = "button13";
+            this.Button13.MatchedIndex = 0;
+            this.Button13.MsaaName = "-";
+            this.Button13.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button13.Name = "Button13";
+            this.Button13.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button13.ObjectImage")));
+            this.Button13.Parent = this.Form1;
+            this.Button13.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button13.UseCoordinatesOnClick = false;
+            this.Button13.WindowClass = "";
+            this.Button13.WindowText = "-";
+            // 
+            // Button18
+            // 
+            this.Button18.Comment = null;
+            this.Button18.Index = 4;
+            this.Button18.InstanceName = "button18";
+            this.Button18.MatchedIndex = 0;
+            this.Button18.MsaaName = "+";
+            this.Button18.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button18.Name = "Button18";
+            this.Button18.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button18.ObjectImage")));
+            this.Button18.Parent = this.Form1;
+            this.Button18.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button18.UseCoordinatesOnClick = false;
+            this.Button18.WindowClass = "";
+            this.Button18.WindowText = "+";
+            // 
+            // Button16
+            // 
+            this.Button16.Comment = null;
+            this.Button16.Index = 2;
+            this.Button16.InstanceName = "button16";
+            this.Button16.MatchedIndex = 0;
+            this.Button16.MsaaName = "=";
+            this.Button16.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button16.Name = "Button16";
+            this.Button16.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button16.ObjectImage")));
+            this.Button16.Parent = this.Form1;
+            this.Button16.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button16.UseCoordinatesOnClick = false;
+            this.Button16.WindowClass = "";
+            this.Button16.WindowText = "=";
+            // 
+            // Button6
+            // 
+            this.Button6.Comment = null;
+            this.Button6.Index = 10;
+            this.Button6.InstanceName = "button6";
+            this.Button6.MatchedIndex = 0;
+            this.Button6.MsaaName = "C";
+            this.Button6.MsaaRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.Button6.Name = "Button6";
+            this.Button6.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("Button6.ObjectImage")));
+            this.Button6.Parent = this.Form1;
+            this.Button6.UIObjectType = TestAutomationFX.UI.UIObjectTypes.Button;
+            this.Button6.UseCoordinatesOnClick = false;
+            this.Button6.WindowClass = "";
+            this.Button6.WindowText = "C";
+            // 
+            // TextBox_Result
+            // 
+            this.TextBox_Result.Comment = null;
+            this.TextBox_Result.Index = 1;
+            this.TextBox_Result.InstanceName = "textBox_Result";
+            this.TextBox_Result.MatchedIndex = 0;
+            this.TextBox_Result.MsaaName = null;
+            this.TextBox_Result.MsaaRole = System.Windows.Forms.AccessibleRole.Text;
+            this.TextBox_Result.Name = "TextBox_Result";
+            this.TextBox_Result.ObjectImage = ((System.Drawing.Bitmap)(resources.GetObject("TextBox_Result.ObjectImage")));
+            this.TextBox_Result.Parent = this.Form1;
+            this.TextBox_Result.UIObjectType = TestAutomationFX.UI.UIObjectTypes.TextBox;
+            this.TextBox_Result.UseCoordinatesOnClick = true;
+            this.TextBox_Result.WindowClass = "";
+            this.TextBox_Result.WindowText = "0";
+            // 
+            // TestFixture1
+            // 
+            this.UIMapObjectApplications.Add(this.CalculatorApplication);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private TestAutomationFX.UI.UIApplication CalculatorApplication;
+        private TestAutomationFX.UI.UIWindow Form1;
+        private TestAutomationFX.UI.UIWindow Button15;
+        private TestAutomationFX.UI.UIWindow Button14;
+        private TestAutomationFX.UI.UIWindow Button12;
+        private TestAutomationFX.UI.UIWindow Button10;
+        private TestAutomationFX.UI.UIWindow Button9;
+        private TestAutomationFX.UI.UIWindow Button7;
+        private TestAutomationFX.UI.UIWindow Button1;
+        private TestAutomationFX.UI.UIWindow Button2;
+        private TestAutomationFX.UI.UIWindow Button4;
+        private TestAutomationFX.UI.UIWindow Button20;
+        private TestAutomationFX.UI.UIWindow Button3;
+        private TestAutomationFX.UI.UIWindow Button8;
+        private TestAutomationFX.UI.UIWindow Button13;
+        private TestAutomationFX.UI.UIWindow Button18;
+        private TestAutomationFX.UI.UIWindow Button16;
+        private TestAutomationFX.UI.UIWindow Button6;
+        private TestAutomationFX.UI.UIWindow TextBox_Result;
+
+    }
+}
---
+++ calculator-automate/calculatornew/TestFixture1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using TestAutomationFX.Core;
+using TestAutomationFX.UI;
+
+namespace calculatornew
+{
+    [UITestFixture]
+    public partial class TestFixture1 : UIMap
+    {
+        public TestFixture1()
+        {
+            InitializeComponent();
+        }
+
+        public TestFixture1(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+       //  String str = "";
+        //String str2 = "";
+
+        [UITest()]
+        public void autoamteadd()
+    {
+      
+
+        string[] file = File.ReadAllLines("C:/Users/Akash.Rawat/Documents/Visual Studio 2012/Projects/calculatornew/caltest.txt");
+           
+         
+         
+     
+        Button6.Click();
+            int max = 0;
+            int idx = 0;
+           // while (max < file.Length - 1)
+           // {
+            try{
+
+                for (int i = 0; i < file[max].Length & max <= file.Length - 1; i++)
+                {
+                    idx = i;
+                    foreach (char str in file[max])
+                    {
+
+                        if (str == '=')
+                        {
+                            Button16.Click();
+
+                            break;
+                        }
+                        else if (str == '1')
+                        {
+                            Button15.Click();
+
+
+                        }
+                        else if (str == '2')
+                        {
+                            Button14.Click();
+
+
+                        }
+                        else if (str == '3')
+                        {
+                            Button12.Click();
+
+
+                        }
+                        else if (str == '4')
+                        {
+                            Button10.Click();
+
+
+                        }
+                        else if (str == '5')
+                        {
+                            Button9.Click();
+
+
+                        }
+                        else if (str == '6')
+                        {
+                            Button7.Click();
+
+                        }
+                        else if (str == '7')
+                        {
+                            Button1.Click();
+
+
+                        }
+                        else if (str == '8')
+                        {
+                            Button2.Click();
+
+
+                        }
+                        else if (str == '9')
+                        {
+                            Button4.Click();
+
+
+                        }
+                        else if (str == '+')
+                        {
+                            Button18.Click();
+
+
+                        }
+                        else if (str == '-')
+                        {
+                            Button13.Click();
+
+
+                        }
+                        else if (str == '*')
+                        {
+                            Button8.Click();
+
+
+                        }
+                        else if (str == '/')
+                        {
+                            Button3.Click();
+
+
+                        }
+                        else if (str == 'C')
+                        {
+                            Button6.Click();
+
+
+                        }
+                    }
+                    TextBox_Result.VerifyProperty("Text", file[max].Substring(file[max].IndexOf("=") + 1).ToString());
+                    Button6.Click();
+                    max++;
+                }
+
+                
+               
+            }
+
+               
+        //}
+            catch(Exception e)
+            {
+                System.IO.StreamWriter file1 = new StreamWriter(@"E:\automationtestcases\calresult.txt");
+                file1.Write("Failed  => ");
+               
+                file1.WriteLine();
+                file1.Close();
+            }
+    }
+
+        
+
+
+    }
+}
+
